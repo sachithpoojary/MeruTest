37a9b09a3cb5f647b2058ec7ca3ca59ddbfbe608
Sachith Poojary <sachithpoojary@gmail.com>
2020-05-01 16:40:44 +0530

    added get cust qpi

---
+++ MeruTest/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using MeruTest.Models;
+using MeruTest.ResponseModel;
+using MeruTest.ResponseModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeruTest.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly DBContext _dBContext;
+
+        public CustomerController(DBContext dBContext)
+        {
+            this._dBContext = dBContext;
+        }
+
+        [HttpGet]
+        public ActionResult<ResponseModel> Get(string custMob)
+        {
+            try
+            {
+                //SubAreaName, AreaName, CityName, CustomerName, Address
+                var response = (from cus in _dBContext.Customers
+                               join s in _dBContext.SubAreas on cus.SubAreaId equals s.SubAreaId
+                               join a in _dBContext.Areas on s.AreaId equals a.AreaId
+                               join c in _dBContext.Cities on a.CityId equals c.CityId
+                               where cus.CustMobNo == custMob
+                               select new ResponseModel(s.SubAreaName, a.AreaName, c.CityName, cus.CustName, cus.Address))
+                               .FirstOrDefault();
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+    }
+}
---
+++ MeruTest/ResponseModel/ResponseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeruTest.ResponseModels
+{
+    public class ResponseModel
+    {
+        public ResponseModel(string subAreaName, string areaName, string cityName, string customerName, string address)
+        {
+            SubAreaName = subAreaName;
+            AreaName = areaName;
+            CityName = cityName;
+            CustomerName = customerName;
+            Address = address;
+        }
+
+        //SubAreaName, AreaName, CityName, CustomerName, Address
+        public string SubAreaName { get; set; }
+        public string AreaName { get; set; }
+        public string CityName { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+    }
+}
